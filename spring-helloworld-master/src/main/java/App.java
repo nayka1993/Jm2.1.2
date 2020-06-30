65f74212cb07b127234527f349c6a227d7a3f580
nayka1993 <cykaa95@mail.ru>
2020-06-30 11:30:24 +0300

    Sea

---
+++ spring-helloworld-master/src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Сравнение бинов helloworld : " + bean.equals(bean2));
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравнение бинов cat : " + catBean.equals(catBean2));
+    }
+}
